70815d24431ff4917b026eb2956cacc907db549e
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-24 16:43:07 +0530

    Push elements in stack using Linked List

---
+++ StackQueue/StackQueue/Node.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public class Stack
+    {
+        Node top;
+
+        public void Push(int data)
+        {
+            Node Node = new Node(data);
+            if(top == null)
+            {
+                top = Node;
+            }
+            else
+            {
+                Node.next = top;
+                top = Node;
+            }
+        }
+        public void DisplayStack()
+        {
+            Node temp = top;
+           // int count = 0;
+            while (temp != null)
+            {
+               // count++;
+                Console.WriteLine("{0}", temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
